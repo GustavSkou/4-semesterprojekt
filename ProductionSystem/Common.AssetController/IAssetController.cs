6247241401dbfcaefa72d343410c40b7ae0cc00a
Gustav Skou Rasmussen <Gustav.skou.rasmussen@gmail.com>
2026-03-25 13:58:09 +0100

    summary

--- ProductionSystem/Common.AssetController/IAssetController.cs
+++ ProductionSystem/Common.AssetController/IAssetController.cs
@@ -5,6 +5,11 @@ public interface IAssetController
 {
 	public Task<bool> Connect();
 	public Task<bool> Disconnect();
+	/// <summary>
+	/// Returns true if the command was successfully executed
+	/// </summary>
+	/// <param name="command"></param>
+	/// <returns></returns>
 	public Task<bool> SendCommand(AssetCommand command);
 
 	// To notify "ProductHandler" that new production data is avaliable, use ".invoke" on the eventhandler 
