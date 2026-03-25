863fe66ee4b693b5f84a4d916ea379d74814e218
Gustav Skou Rasmussen <Gustav.skou.rasmussen@gmail.com>
2026-03-25 13:45:59 +0100

    sendcommand returns boolean, to indicate if the command was a success or not

--- ProductionSystem/Common.AssetController/IAssetController.cs
+++ ProductionSystem/Common.AssetController/IAssetController.cs
@@ -5,7 +5,7 @@ public interface IAssetController
 {
 	public Task<bool> Connect();
 	public Task<bool> Disconnect();
-	public Task SendCommand(AssetCommand command);
+	public Task<bool> SendCommand(AssetCommand command);
 
 	// To notify "ProductHandler" that new production data is avaliable, use ".invoke" on the eventhandler 
 	public event EventHandler<ProductionEvent> ProductionEventHandler;
