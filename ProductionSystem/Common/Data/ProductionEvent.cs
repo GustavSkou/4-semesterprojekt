a4385ee7c73bef117a00fd33fbe2d0fcd84b7c8f
Gustav Skou Rasmussen <Gustav.skou.rasmussen@gmail.com>
2026-03-25 13:43:58 +0100

    tostring

--- ProductionSystem/Common/Data/ProductionEvent.cs
+++ ProductionSystem/Common/Data/ProductionEvent.cs
@@ -7,4 +7,9 @@ public class ProductionEvent
     public string? Source { get; set; }
     public string? Type { get; set; }
     public string? Level { get; set; }
+
+    public override string ToString()
+    {
+        return $"DateAndTime: {DateAndTime},\nDescription: {Description},\nSource: {Source},\nType: {Type},\nLevel: {Level}";
+    }
 }
