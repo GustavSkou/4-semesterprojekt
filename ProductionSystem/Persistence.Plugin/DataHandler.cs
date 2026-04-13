5b8579fc0175e5162d6e73c2a7374d2ba7601d79
Gustav Skou Rasmussen <Gustav.skou.rasmussen@gmail.com>
2026-04-13 11:25:39 +0200

    create db context

--- ProductionSystem/Persistence.Plugin/DataHandler.cs
+++ ProductionSystem/Persistence.Plugin/DataHandler.cs
@@ -1,16 +1,80 @@
-using System.Net.ServerSentEvents;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using Common.Data;
 using Common.Presistence;
+using Microsoft.EntityFrameworkCore;
+using PersistencePlugin.Models;
 
 namespace PersistencePlugin;
 
 public class DataHandler : IPersistence
 {
+    private readonly DbContextOptions<ProductionDbContext> _dbOptions;
+
+    public DataHandler()
+    {
+        var connectionString = "Host=localhost;Port=5433;Database=configurepc;Username=configurepc;Password=configurepc";
+
+        _dbOptions = new DbContextOptionsBuilder<ProductionDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+    }
+
     public void SaveProductionEvent(ProductionEvent productionEvent)
     {
-        throw new NotImplementedException();
+        /*
+        using var dbContext = new ProductionDbContext(_dbOptions);
+        
+        source source = new source();
+        level level = new level();
+        type type = new type();
+
+        if (!string.IsNullOrEmpty(productionEvent.Source))
+        {
+            source.
+            {
+                name = productionEvent.Source
+            };
+        }
+
+        if (!string.IsNullOrEmpty(productionEvent.Level))
+        {
+            level 
+            {
+                name = productionEvent.Level
+            };
+        }
+
+        if (!string.IsNullOrEmpty(productionEvent.Type))
+        {
+            type type = new type
+            {
+                name = productionEvent.Type
+            };
+        }
+        
+
+
+        log log = new log
+        {
+            timestamp   = productionEvent.DateAndTime,
+            description = productionEvent.Description,
+            level_id    = 
+            source_id   =
+            type_id     =
+            level =
+            source =
+
+type=;
+        };
+
+        dbContext.logs.save
+        
+
+        /*productionEvent.Type;
+        productionEvent.Level;
+        productionEvent.Source;*/
+
+
+
     }
 
     public Item[] GetComponents()
